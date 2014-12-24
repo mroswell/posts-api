3c3d30fb3992e9a45b0c4ed6cd196cc2c3d4ee82
Margie Roswell <mroswell@gmail.com>
2014-12-23 22:14:07 -0500

    first commit since cloned skeleton

--- posts/api.py
+++ posts/api.py
@@ -1,10 +1,78 @@
 import json
 
-from flask import request, Response, url_for
+from flask import request, Response, url_for, redirect
 from jsonschema import validate, ValidationError
 
 import models
 import decorators
 from posts import app
 from database import session
+
+# @app.route("/api/posts", methods=["GET"])
+# def posts_get():
+#     """ Get a list of posts """
+#     data = json.dumps([])
+#     return Response(data, 200, mimetype="application/json")
+
+
+@app.route("/api/posts", methods=["GET"])
+@decorators.accept("application/json")
+def posts_get():
+    # # Get the posts from the database
+    # posts = session.query(models.Post).all()
+
+    """ Get a list of posts """
+    # Get the querystring arguments
+    title_like = request.args.get("title_like")
+
+    # Get and filter the posts from the database
+    posts = session.query(models.Post)
+    if title_like:
+        posts = posts.filter(models.Post.title.contains(title_like))
+    posts = posts.all()
+
+    # Convert the posts to JSON and return a response
+    data = json.dumps([post.as_dictionary() for post in posts])
+    return Response(data, 200, mimetype="application/json")
+
+@app.route("/api/posts/<int:id>", methods=["GET"])
+@decorators.accept("application/json")
+def post_get(id):
+    """ Single post endpoint """
+    # Get the post from the database
+    post = session.query(models.Post).get(id)
+
+    # Check whether the post exists
+    # If not return a 404 with a helpful message
+    if not post:
+        message = "Could not find post with id {}".format(id)
+        data = json.dumps({"message": message})
+        return Response(data, 404, mimetype="application/json")
+
+    # Return the post as JSON
+    data = json.dumps(post.as_dictionary())
+    return Response(data, 200, mimetype="application/json")
+
+# @app.route("/posts/<int:id>/delete")
+# @login_required
+# def delete_post_get(id):
+#     post = session.query(Post).get(id)
+#     session.delete(post)
+#     session.commit()
+#     return redirect(url_for("posts"))
+
+@app.route("/api/posts/<int:id>/delete", methods=["GET"])
+def delete_post_get(id):
+    post = session.query(models.Post).get(id)
+    # Check whether the post exists
+    # If not return a 404 with a helpful message
+    if not post:
+        message = "Could not find post with id {}".format(id)
+        data = json.dumps({"message": message})
+        return Response(data, 404, mimetype="application/json")
+
+    session.delete(post)
+    session.commit()
+    return redirect(url_for("posts_get"))
+
 
